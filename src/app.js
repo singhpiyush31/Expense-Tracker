bcaa035953db5af373cdea1d418022b4848f0e95
Piyush Singh <piyushkumarsingh.pks31@gmail.com>
2026-07-28 22:17:37 +0530

    Server and Database Setup

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const connectDB = require('./config/database');
+
+const app = express();
+
+
+
+connectDB().then(() => {
+    console.log("Database connected successfully!");
+    app.listen(7777 , () => {
+        console.log("Server is listening successfully on port 7777");
+    });
+}).catch((err) => {
+    console.error("Database not connected!");
+});
