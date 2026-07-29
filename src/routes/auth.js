b5702faac2f6b3af107c975a16f3125e3189dbcb
Piyush Singh <piyushkumarsingh.pks31@gmail.com>
2026-07-29 23:28:33 +0530

    Login API

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
-const { model } = require('mongoose');
 
 const authRouter = express.Router();
 
@@ -18,6 +17,25 @@ authRouter.post("/register", async (req,res) => {
         res.send("User Added Successfully!");
     } catch (err) {
         res.status(500).json({ error: err.message, message: "Internal Server Error" });
+    }
+});
+
+authRouter.post("/login", async (req,res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+
+        if(!user) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+
+        const passwordValid = await bcrypt.compare(password, user.password);
+
+        if(passwordValid) {
+            res.status(200).json({ message: "Login Successfully! "});
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message, message: "Internal Server Error"});
     }
 });
 
